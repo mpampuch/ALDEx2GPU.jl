ab1a750d5b90ee04a636376f54c58f72311257c8
Documenter.jl <documenter@juliadocs.github.io>
2025-12-30 18:46:38 +0000

    build based on 15a2e96

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#ALDEx2GPU","page":"Home","title":"ALDEx2GPU","text":"Documentation for ALDEx2GPU.\n\n","category":"section"}]
+[{"location":"#ALDEx2GPU","page":"Home","title":"ALDEx2GPU","text":"Documentation for ALDEx2GPU.\n\n","category":"section"},{"location":"#ALDEx2GPU.hello_world_gpu-Tuple{}","page":"Home","title":"ALDEx2GPU.hello_world_gpu","text":"hello_world_gpu()\n\nPrint a greeting message. Used for testing package import to R.\n\n\n\n\n\n","category":"method"}]
 }
