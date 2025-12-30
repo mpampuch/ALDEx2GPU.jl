15a2e9666cb87373eb8297314b0c3e268f78fca0
mpampuch <markpampuch@gmail.com>
2025-12-30 12:41:57 -0600

    Modified package to allow enable the package to be called from README
    - Manually removed dependency versions from [compat] dependencies in Project.toml
    - Manually changed the Julia version to 1.9.4 in [compat] section of Project.toml
    - Added hello_world and hello_world! functions to the package to test that the package can be called from README

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://mpampuch.github.io/ALDEx2GPU.jl/dev/)
 [![Build Status](https://github.com/mpampuch/ALDEx2GPU.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/mpampuch/ALDEx2GPU.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/mpampuch/ALDEx2GPU.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/mpampuch/ALDEx2GPU.jl)
+
+Run with:
+
+```bash
+julia +1.10.10 --project=.
+```
--- src/ALDEx2GPU.jl
+++ src/ALDEx2GPU.jl
@@ -2,4 +2,26 @@ module ALDEx2GPU
 
 # Write your package code here.
 
+"""
+    hello_world_gpu()
+
+Print a greeting message. Used for testing package import to R.
+"""
+function hello_world_gpu()
+    return "Hello World from ALDEx2GPU.jl!"
 end
+
+# Add a function with a ! in the name to test that the R wrapper converts it to a _bang
+function hello_world_gpu!()
+    return "Hello World with a !BANG! in the name from ALDEx2GPU.jl!"
+end
+
+export hello_world_gpu
+export hello_world_gpu!
+
+end
+
+
+
+
+
